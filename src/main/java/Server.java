b89711b4f05c3b4ae07ba378415a0e8472eaac77
Natalia Zubareva <n.zubareva@inbox.ru>
2022-04-26 08:26:02 +0300

    added try-with-resources

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,31 +7,34 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Server started");
         int port = 8089;
-        ServerSocket serverSocket = new ServerSocket(port);
 
         while (true) {
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d. Are you child? (yes/no)", name, clientSocket.getPort()));
-            final String answer1 = in.readLine();
-            System.out.println(answer1);
-            if (answer1.equals("yes")) {
-                out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
-            } else if (answer1.equals("no")) {
-                out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
-            } else {
-                out.println("The answer is wrong");
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+            {
+                System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d. Are you child? (yes/no)", name, clientSocket.getPort()));
+                final String answer1 = in.readLine();
+                System.out.println(answer1);
+                if (answer1.equals("yes")) {
+                    out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                } else if (answer1.equals("no")) {
+                    out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                } else {
+                    out.println("The answer is wrong");
+                }
+                final String bye = in.readLine();
+                System.out.println(bye);
+                out.println(bye);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            final String bye = in.readLine();
-            System.out.println(bye);
-            out.println(bye);
         }
     }
 
